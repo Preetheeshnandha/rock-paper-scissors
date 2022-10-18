81027cc602a2a9b84a4a696774fc9d5ae99d100b
Preetheeshnandha <preetheeshnandha@gmail.com>
2022-10-18 16:45:52 +0530

    Add game function and edit in other functions.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
+let playerSelection;
+let computerSelection;
+let playerCount;
+let computerCount; 
+
+console.log(game());
 
 
 function getComputerChoice(){
@@ -7,23 +13,54 @@ function getComputerChoice(){
     return choice[i];
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
-console.log("computer "+computerSelection);
-console.log("player "+playerSelection);
-console.log(makeDecision(playerSelection,computerSelection));
+
 
 function makeDecision(playerSelection, computerSelection){
-
+    
 if(playerSelection == computerSelection){
+    playerCount = false;
+    computerCount = false;
     return "This round is tie";
 }else if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper"){
+    playerCount = true;
+    computerCount = false;
     return "you win, "+ playerSelection +" beats "+computerSelection;
 }else if(computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "paper"){
+    computerCount = true; 
+    playerCount = false;
     return "you lose, "+ computerSelection +" beats "+playerSelection;
 }
-
-
 }
 
+function game(){
+    
+   let playerScore=0;
+   let computerScore=0;
 
+    for(let i=0;i < 5; i++){
+        playerSelection = prompt("Enter the value");
+        computerSelection = getComputerChoice();
+        
+        makeDecision(playerSelection, computerSelection);
+        if (playerCount){
+            playerScore += 1;
+        }else if(computerCount){
+            computerScore += 1;
+        }
+        
+        console.log(playerScore);
+        console.log(computerScore);
+
+        console.log("computer "+computerSelection);
+        console.log("player "+playerSelection);
+        console.log(makeDecision(playerSelection,computerSelection));
+    }
+
+if(playerScore == computerScore){
+    return "Both are tie in this match";
+}else if(playerScore > computerScore){
+    return "player score "+playerScore+" computerScore "+computerScore+"           Player winner";
+}else if(playerScore < computerScore){
+    return "player score "+playerScore+" computerScore "+computerScore+"           Computer winner!";
+}
+}
